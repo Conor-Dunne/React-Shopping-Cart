828fcee2be5bb2e71814764fc9eb71655326cd75
Conor Dunne <conordunne@outlook.ie>
2023-01-19 23:42:13 +0900

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav';
+import {Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
     <Nav />
+    <Routes>
+      <Route path='/' element={null} />
+      <Route path='/store' element={null} />
+      <Route path='/cart' element={null} />
+    </Routes>
     </div>
   );
 }
