a053203fe5acfbbae1e69c2697f82c3dab0f066c
Conor Dunne <conordunne@outlook.ie>
2023-01-20 00:35:23 +0900

    Test pico css

--- src/Nav.js
+++ src/Nav.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
     return (
-        <nav>
+        <nav className="container-fluid">
             <Link to="/" >Home</Link>
             <Link to="/store" >Store</Link>
             <Link to="/cart" >Cart</Link>
