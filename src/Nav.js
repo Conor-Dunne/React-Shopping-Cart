c1963d5679c04bb11b8371daa2b196cb11029e72
Conor Dunne <conordunne@outlook.ie>
2023-01-19 23:38:04 +0900

    Add links

--- src/Nav.js
+++ src/Nav.js
@@ -4,6 +4,8 @@ export default function Nav() {
     return (
         <nav>
             <Link to="/" >Home</Link>
+            <Link to="/store" >Store</Link>
+            <Link to="/cart" >Cart</Link>
         </nav>
     )
 }
