a84b165669f16efc8f508d870292139db2225925
Conor Dunne <conordunne@outlook.ie>
2023-01-20 00:13:16 +0900

    Tried pico css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
 import Nav from './Nav';
 import {Routes, Route} from 'react-router-dom';
-
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
     <Nav />
     <Routes>
-      <Route path='/' element={null} />
+      <Route path='/' element={<Home />} />
       <Route path='/store' element={null} />
       <Route path='/cart' element={null} />
     </Routes>
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "pico-css";
+
+
+export default function Home () {
+    return (
+        <main className="container">
+            <h1>Welcome to our store!</h1>
+        </main>
+    )
+}
